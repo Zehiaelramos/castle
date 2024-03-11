5378a3a1291d1a4f65d0d693b6dbb0afa93a3a52
Tyrell Wellick <twellick@ecorp.dev>
2024-03-11 14:15:35 -0600

    Add appliances module

---
+++ resources/js/Pages/Appliance.vue
@@ -0,0 +1,68 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Aparatos" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Aparatos</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">You're logged in!</div>
+                </div>
+            </div>
+        </div>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">You're logged in!</div>
+                </div>
+            </div>
+        </div>
+
+        <table class="table">
+            <thead>
+                <tr v-for="appliance in appliances" :key="appliance.id"></tr>
+                <td class="py-0 text-center">{{ appliance.id }}</td>
+            </thead>
+        </table>
+
+    </AuthenticatedLayout>
+</template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+    name: 'ApplianceComponent',
+    data() {
+      return {
+        appliance: {},
+        appliances: {
+          id: '',
+          name: '',
+          description: '',
+        },
+      };
+    },
+    created() {
+      this.getAppliances()
+    },
+    methods: {
+      getAppliances() {
+        axios.get('/appliances')
+          .then(resp => {
+            this.appliances = resp.data
+            console.log('this.appliances: ', resp.data)
+          })
+      }
+    }
+  }
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ApplianceController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -28,6 +29,12 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/appliance', function () {
+    return Inertia::render('Appliance');
+})->middleware(['auth', 'verified'])->name('appliance');
+
+Route::get('/appliances', [ApplianceController::class, 'index']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
